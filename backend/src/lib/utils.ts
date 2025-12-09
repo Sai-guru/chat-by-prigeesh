5c53c12c23e801ca8903552b481e1b0009a1ffde
Sai-guru <lpnkaveri@gmail.com>
2025-12-09 18:36:07 +0530

    fixed env issues

--- backend/src/lib/utils.ts
+++ backend/src/lib/utils.ts
@@ -1,23 +1,31 @@
-import jwt from 'jsonwebtoken';
-import { Response } from 'express';
+    import jwt from 'jsonwebtoken';
+    import { Response } from 'express';
 
-import { ENV } from './env.js';
+    import { ENV } from './env.js';
 
-export const generateToken = (userId: any,res : Response) => {
-    const token =  jwt.sign({ userId }, ENV.JWT_SECRET as string, { expiresIn: '6d' });
-    res.cookie('jwt', token, {
-        httpOnly: true,
-        sameSite: 'strict',
-        secure: ENV.NODE_ENV === 'development' ? false : true,
-        maxAge: 6 * 24 * 60 * 60 * 1000, // 6 days
-    });
-    return token;
-}
+    export const generateToken = (userId: any,res : Response) => {
+        const token =  jwt.sign({ userId }, ENV.JWT_SECRET as string, { expiresIn: '6d' });
+        // res.cookie('jwt', token, {
+        //     httpOnly: true,
+        //     sameSite: 'strict',
+        //     // secure: ENV.NODE_ENV === 'development' ? false : true,
+        //     secure: ENV.NODE_ENV === 'production' ? true : false,
+        //     maxAge: 6 * 24 * 60 * 60 * 1000, // 6 days
+        // });
+        res.cookie('jwt', token, {
+    httpOnly: true,
+    secure: ENV.NODE_ENV === 'production',   // HTTPS only in prod
+    sameSite: ENV.NODE_ENV === 'production' ? 'none' : 'lax', // cross-origin in prod
+    maxAge: 6 * 24 * 60 * 60 * 1000, // 6 days
+});
 
-export const verifyToken = (token: string) => {
-    try {
-        return jwt.verify(token, ENV.JWT_SECRET as string);
-    } catch (error) {
-        return null;
+        return token;
     }
-}
+
+    export const verifyToken = (token: string) => {
+        try {
+            return jwt.verify(token, ENV.JWT_SECRET as string);
+        } catch (error) {
+            return null;
+        }
+    }
