a4abbf0628ea3cddee2bac335f11d63fdf18fa92
Sai-guru <lpnkaveri@gmail.com>
2025-12-09 19:36:24 +0530

    fixed build issues

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -8,9 +8,12 @@ import axios from "axios";
 //   // For production, use the VITE_API_URL if set, otherwise use /api
 //   return import.meta.env.VITE_BACKEND_URL + "/api";
 // };
-const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000"
+    : ""; // empty string for same-origin in production -actuall i dont even need it yooo
 
 export const axiosInstance = axios.create({
-  baseURL:`${BASE_URL}/api`,
+  baseURL: `${BASE_URL}/api`,
   withCredentials: true,
 });
