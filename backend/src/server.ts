f4c59d5d781084236f0ef912674df1d05a3f2aeb
Sai-guru <lpnkaveri@gmail.com>
2025-12-09 20:13:54 +0530

    made prod ready

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -27,12 +27,12 @@ app.use("/api/messages", messageRoutes);
 const __dirname = path.resolve();
 
 if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "./frontend/dist")));
 
   // Express 5 compatible catch-all
   // Catch all remaining routes and serve index.html
   app.use((req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+    res.sendFile(path.join(__dirname, "./frontend/dist", "index.html"));
   });
 }
 
